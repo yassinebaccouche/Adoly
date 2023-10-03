ac579c7881e65d2630acb36c857e8ee56fa3a900
yessine baccouche <72131564+yassinebaccouche@users.noreply.github.com>
2023-10-03 23:31:30 +0100

    Innitial commit

--- android/app/src/main/kotlin/com/example/adol/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.adol
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/screens/Quiz/utils/bindings_app.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:adol/screens/Quiz/controller/quiz_controller.dart';
-
-class BilndingsApp implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => QuizController());
-  }
-}
